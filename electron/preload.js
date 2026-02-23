649f3456a8f807907bba6d0e795bbe8e08c040d1
ReyeMx96 <reyefreak716@gmail.com>
2026-02-23 05:56:19 -0600

    Agrego build versión 0.0.2

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,6 @@
 const { contextBridge, ipcRenderer } = require("electron");
+// Esto te permite ver si el preload se cargó
+console.log("Preload.js se está ejecutando");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   imprimirTicket: (html) => ipcRenderer.invoke("imprimir-ticket", html)
